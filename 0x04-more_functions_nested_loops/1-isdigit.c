0057812af88d32c7f5da2aad6cd7e6cef1919a23
Iqmah30 <ayankunleoyindamola30@gmail.com>
2022-03-17 20:34:32 -0700

    Updated

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+ (12 sloc)  199 Bytes
+  
+/**
+ * main - check the code for digits 0 to 9
+ * Return: Always 0.
+ * @c: The character to be checked
+ */
+int _isdigit(int c);
+{
+	int i;
+
+	for (c = 0; c <= 9; c++)
+		return (1);
+	else
+		return (0);
